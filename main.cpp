834cf21c649da32be24f9847228c1aead261065e
MiBeloz <miha_bmv_@mail.ru>
2023-05-24 16:17:24 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "node.h"
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    std::cout << "\tУстранение пересечения\n" << std::endl;
+
+    {
+        auto node1 = std::make_shared<node>(1);
+        auto node2 = std::make_shared<node>(2);
+
+        //Строка, приводящая к пересечению
+        //node1->parent = node2;
+
+        node2->parent = node1;
+    }
+
+    system("pause > nul");
+    return 0;
+}
---
+++ node.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <iostream>
+#include <memory>
+
+
+class node
+{
+public:
+    int m_value;
+    std::shared_ptr<node> parent;
+
+    node(int value) : m_value{ value } {};
+
+    ~node() { std::cout << "destructor called\n"; }
+};
